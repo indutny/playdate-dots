8fecab9be82f5bd3c34720cd295e74ef69641dbe
Fedor Indutny <238531+indutny@users.noreply.github.com>
2023-07-14 13:51:03 -0700

    refactor

--- source/main.lua
+++ source/main.lua
@@ -4,30 +4,17 @@ import "CoreLibs/sprites"
 import "CoreLibs/timer"
 import "CoreLibs/ui"
 
-import "Bucket"
-import "Food"
+import "scenes/Game"
 
-local MAX_LIFE <const> = 5
 local gfx <const> = playdate.graphics
 
-local buckets = nil
-local food = nil
-local foodTimer = nil
-local score = nil
-local life = nil
-
-function addFood()
-  table.insert(food, Food(math.random(1, 4)))
-end
+local scene = nil
 
 function initGame()
   math.randomseed(playdate.getSecondsSinceEpoch())
   playdate.ui.crankIndicator:start()
 
-  buckets = {Bucket(0), Bucket(90), Bucket(180), Bucket(270)}
-  food = {}
-  score = 0
-  life = MAX_LIFE
+  scene = Game()
 
   foodTimer = playdate.timer.keyRepeatTimerWithDelay(1000, 1000, addFood)
 
@@ -37,77 +24,7 @@ end
 initGame()
 
 function playdate.update()
-  local crank = playdate.getCrankPosition()
-
-  -- Update objects
-
-  for _, bucket in ipairs(buckets) do
-    if bucket ~= nil then
-      bucket:setAngle(crank)
-    end
-  end
-
-  for i = #food, 1, -1 do
-    local f = food[i]
-    local b = buckets[f.row]
-
-    f:move(b:isOpen())
-
-    if f:isDead() then
-      if f.isConsumed then
-        -- TODO(indutny): sound
-        b:feed()
-        score += 1
-      else
-        life -= 1
-      end
-      table.remove(food, i)
-    end
-  end
-
-  for row = #buckets, 1, -1 do
-    local bucket = buckets[row]
-    if bucket:isFull() then
-      -- TODO(indutny): sound and animation
-      local newBucket = Bucket(math.random(0, 3) * 90)
-      newBucket:setAngle(crank)
-      buckets[row] = newBucket
-      if life < MAX_LIFE then
-        life += 1
-      end
-
-      -- Remove all food on this row
-      for i = #food, 1, -1 do
-        local f = food[i]
-        if f.row == row then
-          table.remove(food, i)
-        end
-      end
-    end
-  end
-
-  -- Draw objects
-
-  gfx.clear()
-
-  for row, bucket in ipairs(buckets) do
-    bucket:draw(row)
-  end
-  for _, f in ipairs(food) do
-    f:draw()
-  end
-
-  gfx.drawTextAligned(
-    "*Score: " .. tostring(score) .. "*",
-    398,
-    2,
-    kTextAlignment.right)
-
-  gfx.drawTextAligned(
-    "*Life: " .. tostring(life) .. "*",
-    398,
-    222,
-    kTextAlignment.right)
+  scene:update()
 
   -- Update internal playdate state
 
---
+++ source/scenes/Game.lua
@@ -0,0 +1,107 @@
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
+import "CoreLibs/ui"
+
+import "objects/Bucket"
+import "objects/Food"
+
+local MAX_LIFE <const> = 5
+local gfx <const> = playdate.graphics
+
+class("Game").extends()
+
+function Game:init()
+  Game.super.init(self)
+
+  self.buckets = {Bucket(0), Bucket(90), Bucket(180), Bucket(270)}
+  self.food = {}
+  self.score = 0
+  self.life = MAX_LIFE
+
+  self.foodTimer = playdate.timer.keyRepeatTimerWithDelay(1000, 1000, Game.addFood, self)
+end
+
+function Game:remove()
+  self.foodTimer:remove()
+end
+
+function Game:addFood()
+  table.insert(self.food, Food(math.random(1, 4)))
+end
+
+function Game:update()
+  local crank = playdate.getCrankPosition()
+
+  -- Update objects
+
+  for _, bucket in ipairs(self.buckets) do
+    if bucket ~= nil then
+      bucket:setAngle(crank)
+    end
+  end
+
+  for i = #self.food, 1, -1 do
+    local f = self.food[i]
+    local b = self.buckets[f.row]
+
+    f:move(b:isOpen())
+
+    if f:isDead() then
+      if f.isConsumed then
+        -- TODO(indutny): sound
+        b:feed()
+        self.score += 1
+      else
+        self.life -= 1
+      end
+      table.remove(self.food, i)
+    end
+  end
+
+  for row = #self.buckets, 1, -1 do
+    local bucket = self.buckets[row]
+    if bucket:isFull() then
+      -- TODO(indutny): sound and animation
+      local newBucket = Bucket(math.random(0, 3) * 90)
+      newBucket:setAngle(crank)
+      self.buckets[row] = newBucket
+      if self.life < MAX_LIFE then
+        self.life += 1
+      end
+
+      -- Remove all food on this row
+      for i = #self.food, 1, -1 do
+        local f = self.food[i]
+        if f.row == row then
+          table.remove(self.food, i)
+        end
+      end
+    end
+  end
+
+  -- Draw objects
+
+  gfx.clear()
+  gfx.setLineWidth(4)
+
+  for row, bucket in ipairs(self.buckets) do
+    bucket:draw(row)
+  end
+  for _, f in ipairs(self.food) do
+    f:draw()
+  end
+
+  gfx.drawTextAligned(
+    "*Score: " .. tostring(self.score) .. "*",
+    398,
+    2,
+    kTextAlignment.right)
+
+  gfx.drawTextAligned(
+    "*Life: " .. tostring(self.life) .. "*",
+    398,
+    222,
+    kTextAlignment.right)
+end
